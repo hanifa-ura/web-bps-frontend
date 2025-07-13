ca807034095f69109430248b69aea0eb28202f9b
hanifa-ura <hanifaaura5008092@gmail.com>
2025-07-13 22:10:46 +0700

    gajadi

--- src/components/EditPublicationPage.jsx
+++ src/components/EditPublicationPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { usePublications } from '../hooks/usePublications';
+import { uploadImageToCloudinary } from '../services/publicationService';
 
 
 export default function EditPublicationPage() {
