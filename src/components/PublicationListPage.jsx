343889c1706db394727c1031491a4ed47c77a3fd
hanifa-ura <hanifaaura5008092@gmail.com>
2025-07-13 23:44:10 +0700

    edit publicationlistpage_2

--- src/components/PublicationListPage.jsx
+++ src/components/PublicationListPage.jsx
@@ -1,5 +1,5 @@
 // src/components/PublicationListPage.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { usePublications } from '../hooks/usePublications';
 import { useNavigate } from 'react-router-dom';
 
